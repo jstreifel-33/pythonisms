fc8d0ba58abfa8a93684b1a8d44d7dc1d2bb45ec
Joe Streifel <josephstreifel@gmail.com>
2022-01-27 19:47:13 -0800

    updates tests to be correct - adds test for __iter__ cast to list

--- tests/test_pythonisms.py
+++ tests/test_pythonisms.py
@@ -1,7 +1,13 @@
 from pythonisms.pythonisms import CoolerLinkedList as LinkedList
 
 
-def test_iter_list_cast():
+def test_linked_list_init():
     li = [1, 2, 3]
     example = LinkedList(li)
     assert str(example) == '{ 1 } -> { 2 } -> { 3 } -> None'
+
+
+def test_linked_list_cast_list():
+    li = [1, 2, 3]
+    example = LinkedList(li)
+    assert list(example) == [1, 2, 3]
